75097e2be9d76fe614a422d7acae0b5ec288fa63
vr1eva <113930888+vr1eva@users.noreply.github.com>
2023-03-30 08:03:54 -0500

    1 binaries uploaded

---
+++ pages/figma.js
@@ -0,0 +1,13 @@
+export default function Figma() {
+  return (
+    <div>
+      <iframe
+        style={{ border: '1px solid rgba(0, 0, 0, 0.1)' }}
+        width="800"
+        height="450"
+        src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2Fy4WuCbX9hbxmMAjFqZesE9%2Fportfolio%3Fnode-id%3D0%253A1%26t%3DRDZuRss7GKUwEXGb-1"
+        allowfullscreen
+      ></iframe>
+    </div>
+  );
+}
